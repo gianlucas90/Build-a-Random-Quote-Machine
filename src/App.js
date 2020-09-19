831b409cccd490ef63e7ebbffaf148a98d828aab
Gianluca <gianlucazin1990@gmail.com>
2020-09-19 09:35:18 +0200

    deleted some files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Reac
+      <div id="quote-box">
+        <h4 id="text">This is the quote text</h4>
+        <p id="author">Author</p>
+        <a href="twitter.com/intent/tweet" id="tweet-quote">
+          Twitter
         </a>
-      </header>
+        <button id="new-quote">New Quote</button>
+      </div>
     </div>
   );
 }
