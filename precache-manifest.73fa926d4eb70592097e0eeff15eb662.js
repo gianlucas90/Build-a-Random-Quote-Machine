765ad86357de04a032079d91ea088d043202de8e
Gianluca <gianlucazin1990@gmail.com>
2020-09-21 14:46:03 +0200

    Updates

---
+++ precache-manifest.73fa926d4eb70592097e0eeff15eb662.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c9f980675fa2c02c6bba1cdf6a89b72",
+    "url": "/Random-quote-machine/index.html"
+  },
+  {
+    "revision": "87e88f0bd35bea1f0308",
+    "url": "/Random-quote-machine/static/css/main.09e94426.chunk.css"
+  },
+  {
+    "revision": "cf5c7fe1234b3056cae4",
+    "url": "/Random-quote-machine/static/js/2.1302592b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Random-quote-machine/static/js/2.1302592b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87e88f0bd35bea1f0308",
+    "url": "/Random-quote-machine/static/js/main.31dc055a.chunk.js"
+  },
+  {
+    "revision": "c784c22d8759fdaf1b7e",
+    "url": "/Random-quote-machine/static/js/runtime-main.7562f72b.js"
+  }
+]);
